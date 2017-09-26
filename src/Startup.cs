3b1d56c2be5eb531feac0ca21d2261129fadca3f
Steve Desmond <steve@vtsv.ca>
2017-09-25 20:19:34 -0400

    show exceptions in prod

--- src/Startup.cs
+++ src/Startup.cs
@@ -48,8 +48,7 @@ namespace GoldenForCongress
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
+            app.UseDeveloperExceptionPage();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
